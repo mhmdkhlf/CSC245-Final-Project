48b8c4c2abc5816376adf3820c9cffa1ceb48a64
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2021-04-17 14:12:12 +0300

    Add files via upload

---
+++ LauUser.java
@@ -0,0 +1,49 @@
+
+public abstract class LauUser {
+	
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String email;
+	
+	public LauUser(String firstName, String lastName, int age,String email) {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setAge(age);
+		setEmail(email);
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public boolean isValidMail() {
+		return email.contains("@lau.edu");
+	} 
+	
+	public String toString() {
+		return String.format("%s %s\n%s",firstName, lastName, email);
+	}
+
+}
