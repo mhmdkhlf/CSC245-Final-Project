650212991ecc8dc620d9718ccf7f11eeff22d3dc
mhmdkhlf <mohammadkhalifeh1010@gmail.com>
2021-04-17 18:21:32 +0300

    Update and rename Test.java to main.java

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class Test
+{	
+	public static void main(String[] args) throws IOException, InvalidEmailException, InvalidPasswordException, UserAlreadySignedUpException
+	{
+// 		Student s1 = new Student("Mohamad", "Khalifeh", "mohamad.khalifeh03@lau.edu", "202000865");
+// 		Student s2 = new Student("ALi", "solh", "ali.solh@lau.edu", "202002432");
+// 		Student s3 = new Student("Rani", "Salman", "rani.salman@lau.edu", "202021235");
+// 		Instructor i = new Instructor("Azzam","Mourad","azzam.mourad.@lau.edu", "321");
+// 		Users.addUser(s1);
+// 		Users.addUser(s2);
+// 		Users.addUser(s3);
+// 		Users.addUser(i);
+// 		ArrayList<LauUser> lauUsers = Users.getUsers();
+// 		System.out.println(lauUsers);
+// 		boolean valid = Users.isValidUser(s1.getEmail()+"fdsfadfs", s1.getPassword()); 
+// 		if (valid)
+// 			System.out.println("You are now logged in");
+// 		else
+// 			System.out.println("Invalid username/password");	
+	}	
+}
